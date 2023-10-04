166b023fee29d2693ec94903842476eafa126c4f
TusharMore <tusharmore2ab@gmail.com>
2023-10-04 19:10:52 +0530

    commited successfully

--- controller/contactControlller.js
+++ controller/contactControlller.js
@@ -1,7 +1,56 @@
 import contactModel from '../models/contactModel.js';
 
 // Create a new user
+
+import nodemailer from 'nodemailer';
+
+// Import your contactModel if it's defined in a separate file
+
 export const createContact = async (req, res) => {
+  try {
+    // Assuming you have a contact model defined
+    const { name, email, mobile, category, message } = req.body;
+    const user = new contactModel({ name, email, mobile, category, message });
+    await user.save();
+
+    // Create a Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: 'GMAIL', // e.g., 'Gmail'
+      auth: {
+        user: 'staff.manasvi@gmail.com', // Your email address
+        pass: 'ufbnyfxdwfygdksq',    // Your email password or app-specific password
+      },
+      tls: {
+        rejectUnauthorized: false, // Disable SSL certificate validation
+      },
+    });
+
+    // Define email data
+    const mailOptions = {
+      from: req.body.email,    // Sender's email address
+      to: 'staff.manasvi@gmail.com', // Recipient's email address
+      subject: 'New Contact Form Submission',
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Mobile: ${mobile}
+        Category: ${category}
+        Message: ${message}
+      `,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+
+/* export const createContact = async (req, res) => {
   try {
     const user = new contactModel(req.body)
     await user.save();
@@ -9,7 +58,7 @@ export const createContact = async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-};
+}; */
 
 // Read all users
 export const ReadContact =  async (req, res) => {
